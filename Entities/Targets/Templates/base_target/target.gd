812d1be4ea8335c641ea650996c4633596092f46
saebyn <me@saebyn.dev>
2025-10-07 20:41:16 -0700

    Add damage_source parameter to _on_health_damaged for improved logging

--- Entities/Targets/Templates/base_target/target.gd
+++ Entities/Targets/Templates/base_target/target.gd
@@ -7,5 +7,5 @@ func _on_died():
   queue_free() # Remove the target from the scene when it dies.
 
 
-func _on_health_damaged(amount: int, hitpoints: int) -> void:
-  Logger.debug("Target.Combat", "Target took %d damage. Remaining HP: %d" % [amount, hitpoints])
+func _on_health_damaged(amount: int, hitpoints: int, damage_source: String = "unknown") -> void:
+  Logger.debug("Target.Combat", "Target took %d damage from %s. Remaining HP: %d" % [amount, damage_source, hitpoints])
