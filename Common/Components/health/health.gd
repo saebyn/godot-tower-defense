570e1fb33739cc70a3a8f90dec030c4e035945d8
saebyn <me@saebyn.dev>
2025-11-30 10:50:03 -0800

    Enhance health component: add disabled property and update display logic

--- Common/Components/health/health.gd
+++ Common/Components/health/health.gd
@@ -2,10 +2,18 @@ extends Node
 class_name Component_Health
 
 @export var hitpoints: int = 100
+@export var disabled: bool:
+  get:
+    return disabled
+  set(value):
+    disabled = value
+    _update_display()
+
 
 @onready var health_bar := $SubViewportContainer/SubViewport/VBoxContainer/HealthBar
 @onready var health_label := $SubViewportContainer/SubViewport/VBoxContainer/HealthLabel
 @onready var subviewport := $SubViewportContainer/SubViewport
+@onready var sprite := $Sprite3D
 
 var max_hitpoints: int
 var dead: bool = false
@@ -14,11 +22,14 @@ signal died(damage_source: String)
 signal damaged(amount: int, hitpoints: int, damage_source: String)
 
 func take_damage(amount: int, damage_source: String = "unknown"):
+  if disabled:
+    return
+
   hitpoints -= amount
   damaged.emit(amount, hitpoints, damage_source)
   _update_display()
   if hitpoints <= 0:
-    die(damage_source)
+    _die(damage_source)
 
 
 func _ready():
@@ -32,12 +43,14 @@ func _ready():
     get_parent().set_meta("health_component", self)
 
 func _update_display():
+  sprite.visible = not disabled
+
   # Set up health display UI
   health_bar.max_value = max_hitpoints
   health_bar.value = hitpoints
   health_label.text = str(hitpoints) + " / " + str(max_hitpoints)
 
-func die(damage_source: String = "unknown"):
+func _die(damage_source: String = "unknown"):
   if dead:
     return
 
